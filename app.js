bac572f6d11f1b7176a1ee177a810eef96e26a39
KevinBermudezC <kevinbermudezc@gmail.com>
2025-02-21 21:05:36 -0500

    initialize ecommerce backend with basic structure and configurations

---
+++ app.js
@@ -0,0 +1,26 @@
+import  express from "express";
+import cookieParser from 'cookie-parser';
+
+import { PORT } from './config/env.js';
+
+import authRouter from "./routes/auth.routes.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(errorMiddleware);
+
+app.use("/auth", authRouter);
+
+
+app.get("/", (req,res) => {
+	res.send("Welcome to Ecommerce Backend API");
+});
+
+app.listen(PORT, () => {
+	console.log(`Ecommerce Backend API is running on http://localhost:${PORT}`);
+})
+
+export default app;
