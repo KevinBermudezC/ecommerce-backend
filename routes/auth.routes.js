576d0234ed037d1c16b27e72826e45b1c35f325f
KevinBermudezC <kevinbermudezc@gmail.com>
2025-02-22 18:13:56 -0500

    implement sign-in and sign-out functionality with JWT authentication

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,18 +1,14 @@
 import {Router} from "express";
-import { signUp } from "../controllers/auth.controllers.js";
+import { signUp,signIn,signOut } from "../controllers/auth.controllers.js";
 
 const authRouter = Router();
 
 
 authRouter.post("/sign-up",signUp);
 
-authRouter.post("/sign-in", (req,res) => {
-	res.send("Sign In Route");
-});
+authRouter.post("/sign-in",signIn);
 
-authRouter.post("/sign-out", (req,res) => {
-	res.send("Sign Out Route");
-});
+authRouter.post("/sign-out",signOut);
 
 
 export default authRouter;
